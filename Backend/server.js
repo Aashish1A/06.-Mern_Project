b1e483134d91d6c438471626d649af639af6127c
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-04-28 23:28:06 +0530

    Completed this mern project

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,27 @@
 import express from "express";
 import dotenv  from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import ProductRoutes from "./routes/product.route.js"
 
-dotenv.config({ path: "../.env" });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json())
 
 app.use("/api/products", ProductRoutes)
+
+// For deployment, serve frontend files
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "Frontend", "dist")))
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"))
+  // })
+}
 
 
 app.listen(PORT, () => {
